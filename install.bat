a6eca2b5e9a2b16c7cc618a6a496f14e4eb55c88
rrryfoo <83335294+mangorifo@users.noreply.github.com>
2022-10-03 13:21:01 +0800

    e
    yes made it unboring cuz u complained about it real 😭
    (totally didnt just add more text)

--- install.bat
+++ install.bat
@@ -1,10 +1,10 @@
-@echo Please wait while CrepeSR is installed...
+@echo Pleaseeeeeeeeee waaaaaait whileeeeeee CrepeSR is being installed...
 
 powershell -Command "Invoke-WebRequest https://cdn.discordapp.com/attachments/983998821786878002/1026344036728061982/QuickServerLaunch.bat -Outfile QuickServerLaunch.bat"
 powershell -Command "Invoke-WebRequest https://cdn.discordapp.com/attachments/983998821786878002/1026344793527304212/temp.bat -Outfile temp.bat"
 START temp.bat
 cls
-@echo You must wait for NPM to install. (It's the window that opened when you ran this) Once that's done, close that window and press any key to continue.
+@echo YOU MUST WAIT for npm to install. (It's the window that opened when you ran this) Once that's done, close that window and press any key to continue.
 
 pause
 
