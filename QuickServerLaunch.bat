374cdae6ce8176d7fa902b43a5e24624ce6cee93
rrryfoo <83335294+mangorifo@users.noreply.github.com>
2022-10-10 14:23:32 +0800

    Update QuickServerLaunch.bat

--- QuickServerLaunch.bat
+++ QuickServerLaunch.bat
@@ -1,4 +1,5 @@
 @echo off
 cd %cd%/CrepePackage
 START mitmstart.bat
+echo If you get "Global Distribution Error", please turn on the MongoDB service, and remember to open MongoDB Compass and connect to a server.
 npm run start
