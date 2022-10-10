16cc5c0406387fd5bbe0c1d464d77342a12f11dd
rrryfoo <83335294+mangorifo@users.noreply.github.com>
2022-10-10 20:27:15 +0800

    Update QuickServerLaunch.bat

--- QuickServerLaunch.bat
+++ QuickServerLaunch.bat
@@ -1,4 +1,5 @@
 @echo off
+title CrepeSR Command Console
 cd %cd%/CrepePackage
 START /commands/mitmstart.bat
 echo If you get "Global Distribution Error", please turn on the MongoDB service, and remember to open MongoDB Compass and connect to a server.
