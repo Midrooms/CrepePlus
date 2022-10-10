289e0a4889d7bce8d844493b93e64b9c59ef9feb
rrryfoo <83335294+mangorifo@users.noreply.github.com>
2022-10-10 20:29:30 +0800

    Update mitmstart.bat

--- commands/mitmstart.bat
+++ commands/mitmstart.bat
@@ -2,4 +2,6 @@ cd %cd%\CrepePackage
 @echo off
 title CrepeSR mitmdump
 echo Do not delete this file. QuickServerLaunch.bat uses this file.
+echo Use commands in "CrepeSR Command Console"
+echo Starting now...
 mitmdump -s proxy.py -k
