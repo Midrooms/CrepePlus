b28445e6e48b9f14f99581551953be61e3ce45b4
rrryfoo <83335294+mangorifo@users.noreply.github.com>
2022-10-10 14:19:08 +0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -4,7 +4,6 @@ cls
 @echo The window that just opened is the NPM installation.
 @echo Please wait for that installation to finish. Close that after, then press any key to continue.
 pause
-START mitmstart.bat
 DEL temp.bat
 DEL readme.md
 DEL install.bat
