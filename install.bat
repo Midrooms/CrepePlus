8a3d39409eff948368b5413dd98e94b76a87978b
rrryfoo <83335294+mangorifo@users.noreply.github.com>
2022-10-10 14:27:25 +0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,7 +2,7 @@
 START temp.bat
 cls
 @echo The window that just opened is the NPM installation.
-@echo Please wait for that installation to finish. Only continue if the installation is complete.
+@echo Please wait for that installation to finish. Only continue if the installation is complete. You might cancel the installation if you continue. (If the installation isn't done yet)
 pause
 DEL temp.bat
 DEL readme.md
