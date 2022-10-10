177d8ce998dcbf8acce6ab9c711c28af42390193
rrryfoo <83335294+mangorifo@users.noreply.github.com>
2022-10-10 14:15:05 +0800

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,6 +1,4 @@
 @echo off
-powershell -Command "Invoke-WebRequest https://cdn.discordapp.com/attachments/983998821786878002/1026344036728061982/QuickServerLaunch.bat -Outfile QuickServerLaunch.bat"
-powershell -Command "Invoke-WebRequest https://cdn.discordapp.com/attachments/983998821786878002/1026344793527304212/temp.bat -Outfile temp.bat"
 START temp.bat
 cls
 @echo The window that just opened is the NPM installation.
