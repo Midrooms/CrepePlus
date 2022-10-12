60d3eac66bd4e560d4153bbf3bdefe10fcc4aab8
rrryfoo <83335294+mangorifo@users.noreply.github.com>
2022-10-12 15:15:40 +0800

    Update and rename help.bat to firstrunmessage.bat

--- commands/firstrunmessage.bat
+++ commands/firstrunmessage.bat
@@ -20,7 +20,9 @@ echo Edit the Address and Port.
 echo Address: 127.0.0.1
 echo Port: 8080
 echo Open MongoDB Compass. Click on "connect"
+echo You might have to start the MongoDB service again every restart.
 echo if it shows "EREFUSED CONNECT" (or something similar), Start the MongoDB Service. (That is, if you installed MongoDB)
+echo Use "startmongo.bat" to start the MongoDB service.
 echo Go to https://github.com/web-pri/crepewiki/wiki/Troubleshooting to find and/or fix more errors that may occur.
 echo Continue if you would like to close this window.
 pause
