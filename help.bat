ef13ac373927be7762ce77c24d8e40b2d4d707a6
rrryfoo <83335294+mangorifo@users.noreply.github.com>
2022-10-12 15:24:51 +0800

    Create help.bat

---
+++ help.bat
@@ -0,0 +1,20 @@
+@echo off
+title CrepePlus (MITM) Help
+cls
+echo Please set proxy settings first, before connecting to Honkai: Star Rail.
+echo Steps:
+echo Go to settings.
+echo Search for "proxy" in the search bar.
+echo Scroll down and find "Use a proxy server".
+echo Turn it on.
+echo Edit the Address and Port.
+echo Address: 127.0.0.1
+echo Port: 8080
+echo Open MongoDB Compass. Click on "connect"
+echo You might have to start the MongoDB service again every restart.
+echo if it shows "EREFUSED CONNECT" (or something similar), Start the MongoDB Service. (That is, if you installed MongoDB)
+echo Use "startmongo.bat" to start the MongoDB service.
+echo Go to https://github.com/web-pri/crepewiki/wiki/Troubleshooting to find and/or fix more errors that may occur.
+pause
+cls
+exit
