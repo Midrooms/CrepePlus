cbaeeda7b47ff8f46227ca942324d046c37c0bd9
rrryfoo <83335294+mangorifo@users.noreply.github.com>
2022-10-13 18:27:20 +0800

    Update startmongo.bat

--- commands/startmongo.bat
+++ commands/startmongo.bat
@@ -1,16 +1,6 @@
 @echo off
 net start MongoDB
 echo Did it start MongoDB?
-echo If it didn't, run this batchfile as administrator.
-echo OR, start it manually, using the Task Manager. Continue if you would like to know how.
-pause
-cls
-start "\Windows\System32\taskmgr.exe"
-echo Click on services, then find "MongoDB"
-pause
-echo Right click on it, then click "Start"
-pause
-cls
-If MongoDB isn't there, then you have to install it yourself.
+echo If it showed an error, that means you do not have MongoDB Community installed.
 pause
 exit
