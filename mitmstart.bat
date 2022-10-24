7d62e40415ba7e556b1bba7b6c3ffcb18dca942c
rrryfoo <83335294+mangorifo@users.noreply.github.com>
2022-10-24 13:28:56 +0800

    Delete mitmstart.bat

--- mitmstart.bat
+++
@@ -1,8 +0,0 @@
-cd %cd%\CrepePackage
-@echo off
-title CrepeSR mitmdump
-echo Do not delete this file. startserver.bat uses this file.
-echo Use commands in the window titled "CrepePlus MITM Console"
-timeout /t 3
-echo Starting now...
-mitmdump -s proxy.py -k
