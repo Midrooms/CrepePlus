581fb3cc74b57a6aaa2d9c854e65f9cd8c643f13
rrryfoo <83335294+mangorifo@users.noreply.github.com>
2022-10-24 13:20:29 +0800

    Update startserver.bat

--- startserver.bat
+++ startserver.bat
@@ -1,7 +1,7 @@
 @echo off
 title CrepePlus MITM Console - v1.4.2 (CrepeSR v1.0)
 cd %cd%\commands
-START /commands/mitmstart.bat
+START /mitmstart.bat
 echo If you get "Global Distribution Error", please turn on the MongoDB service, and remember to open MongoDB Compass and connect to a server.
 cd %cd%/CrepePackage
 npm run start
